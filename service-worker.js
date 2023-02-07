37974a675af5eba56d7da14a9c0f6c78aaab4e3a
samizaman <sxmizaman@gmail.com>
2023-02-07 10:17:18 +0400

    add fetch in service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,3 +17,14 @@ self.addEventListener("install", (e) => {
         })
     );
 });
+
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        // check if the cache has the file
+        caches.match(e.request).then(function (r) {
+            console.log("[ServiceWorker] Fetching resource: " + e.request.url);
+            // 'r' is the matching file if it exists in the cache
+            return r
+        })
+    );
+});
