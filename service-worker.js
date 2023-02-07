5268cd455fdad793dd479523aeb9b61a3ed06a4d
samizaman <sxmizaman@gmail.com>
2023-02-07 10:22:03 +0400

    update fetch in service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,13 +18,31 @@ self.addEventListener("install", (e) => {
     );
 });
 
+// self.addEventListener("fetch", function (e) {
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log("[ServiceWorker] Fetching resource: " + e.request.url);
+//             // 'r' is the matching file if it exists in the cache
+//             return r
+//         })
+//     );
+// });
+
 self.addEventListener("fetch", function (e) {
     e.respondWith(
         // check if the cache has the file
         caches.match(e.request).then(function (r) {
             console.log("[ServiceWorker] Fetching resource: " + e.request.url);
-            // 'r' is the matching file if it exists in the cache
-            return r
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the newly downloaded file to the cache
+                return caches.open(cacheName).then(function (cache) {
+                    console.log("[ServiceWorker] Caching new resource: " + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
